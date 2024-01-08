dbda5e418488b63ae81c96450c6facaaa27b74df
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-09 01:15:53 +0530

    feat: swagger comments for auth

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,8 +3,101 @@ import { login, signup } from "@/controllers/auth.controller";
 
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         _id:
+ *           type: string
+ *           description: The unique identifier for the user
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: The email address of the user
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Auth
+ *   description: Authentication operations
+ */
+
+/**
+ * @swagger
+ * /auth/signup:
+ *   post:
+ *     summary: User registration
+ *     tags: [Auth]
+ *     requestBody:
+ *       description: User registration data
+ *       required: true
+ *       content:
+ *         application/json:
+ *     responses:
+ *       '201':
+ *         description: Successfully registered
+ *         content:
+ *           application/json:
+ *             example:
+ *               data:
+ *                 accessToken: <access_token>
+ *       '400':
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Validation error message
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Cannot signup at the moment. Please try again later.
+ */
 router.post("/signup", signup);
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: User login
+ *     tags: [Auth]
+ *     requestBody:
+ *       description: User login data
+ *       required: true
+ *       content:
+ *         application/json:
+ *     responses:
+ *       '200':
+ *         description: Successfully logged in
+ *         content:
+ *           application/json:
+ *             example:
+ *               data:
+ *                 accessToken: <access_token>
+ *       '400':
+ *         description: Bad Request
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Incorrect email or password
+ *       '500':
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             example:
+ *               error: Something went wrong!
+ */
 router.post("/login", login);
 
 export default router;
