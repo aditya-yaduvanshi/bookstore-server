f92f493c7e7a8c6e2b3f69a6b84bbeacfab0da5d
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-08 17:39:00 +0530

    feat: books model schema

---
+++ src/models/books.model.ts
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+export interface Book {
+  _id: string;
+  title: string;
+  cover: string;
+  description: string;
+  price: number;
+  isPublished: boolean;
+  publishedBy: string;
+  author: string;
+}
+
+const BookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 200,
+    },
+    cover: String,
+    description: String,
+    author: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 64,
+    },
+    price: {
+      type: Number,
+      min: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    publishedBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "users",
+    },
+  },
+  { timestamps: true }
+);
+
+const BookModel = mongoose.model("books", BookSchema);
+
+export default BookModel;
