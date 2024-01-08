31e58a6ec386bda2ad333456a38cf46670cb018c
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-08 17:17:48 +0530

    feat: users model schema

---
+++ src/models/users.model.ts
@@ -0,0 +1,58 @@
+import { createHash } from "lib/bcrypt";
+import mongoose, { CallbackError } from "mongoose";
+
+export interface User {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 64,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 5,
+    },
+    password: String,
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  try {
+    this.password = await createHash(this.password);
+    next();
+  } catch (err) {
+    console.log("pre save:", err);
+    next(err as CallbackError);
+  }
+});
+
+UserSchema.pre(
+  "updateOne",
+  { document: true, query: false },
+  async function (next) {
+    try {
+      if (this.isModified("password"))
+        this.password = await createHash(this.password);
+
+      next();
+    } catch (err) {
+      console.log("pre update one:", err);
+      next(err as CallbackError);
+    }
+  }
+);
+
+const UserModel = mongoose.model("users", UserSchema);
+
+export default UserModel;
